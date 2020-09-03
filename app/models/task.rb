a4af192f2c74d72f437b26a70a9a19f431212ebc
Shawn Kearney <shawnpk29@me.com>
2020-09-03 16:58:15 -0600

    Create STATUS_OPTIONS constant

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ApplicationRecord
   belongs_to :project
 
   validates :status, inclusion: { in: %w[not-started in-progress complete] }
+
+  STATUS_OPTIONS = [
+    ['Not started', 'not-started'],
+    ['In progress', 'in-progress'],
+    ['Complete', 'complete']
+  ]
 end

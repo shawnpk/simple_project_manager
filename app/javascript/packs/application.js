9a7baa74925bf3078127a18b6d562b3f6010418a
Shawn Kearney <shawnpk29@me.com>
2020-09-04 12:02:45 -0600

    Install sortablejs via yarn and configure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start();
 require("channels");
 require("bootstrap/scss/bootstrap");
 
+import Sortable from "sortablejs";
 import "../stylesheets/application";
 
 // Uncomment to copy all static images under ../images to the output folder and reference

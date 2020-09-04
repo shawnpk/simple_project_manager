3547cbcb0f02db6598992c3005fe03081a13064e
Shawn Kearney <shawnpk29@me.com>
2020-09-04 10:36:57 -0600

    Add methods to check task status

--- app/models/task.rb
+++ app/models/task.rb
@@ -19,4 +19,16 @@ class Task < ApplicationRecord
       'success'
     end
   end
+
+  def complete?
+    status == 'complete'
+  end
+
+  def in_progress?
+    status == 'in-progress'
+  end
+
+  def not_started?
+    status == 'not-started'
+  end
 end

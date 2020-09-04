875c97da00551234a0f67d91bae46b7f26878148
Shawn Kearney <shawnpk29@me.com>
2020-09-04 10:36:34 -0600

    Add utility methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,42 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+
+  def badge_color
+    case status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+    end
+  end
+
+  def status
+    return 'not-started' if tasks.none?
+
+    if tasks.all? { |task| task.complete? }
+      'complete'
+    elsif tasks.any? { |task| task.in_progress? || task.complete? }
+      'in-progress'
+    else
+      'not-started'
+    end
+  end
+
+  def percent_complete
+    return 0 if tasks.none?
+
+    complete_tasks = total_complete
+    ((total_complete.to_f / total_tasks) * 100).round
+  end
+
+  def total_complete
+    tasks.select { |task| task.complete? }.count
+  end
+
+  def total_tasks
+    tasks.count
+  end
 end

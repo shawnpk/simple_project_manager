1f9745f9c7ebd517a813554f6f1b347f0f04b86c
Shawn Kearney <shawnpk29@me.com>
2020-09-08 07:39:20 -0600

    Use stimulus morph to render on part of view that has changed

--- app/reflexes/status_reflex.rb
+++ app/reflexes/status_reflex.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class StatusReflex < ApplicationReflex
+  delegate :render, to: ApplicationController
+  
   def change
     task = Task.find(element.dataset[:id])
     task.update(status: element.dataset[:status])
+    morph dom_id(task), render(TaskComponent.new(task: task))
   end
 end
